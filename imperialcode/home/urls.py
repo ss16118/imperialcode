0c57a38f95bd56929eb4118d019e3c4b70286dd7
ht sun <sunhttt@gmail.com>
2020-05-26 23:56:35 +0800

    fixing the css problem, still get (Resource interpreted as Stylesheet but transferred with MIME type application/x-css) error

--- imperialcode/home/urls.py
+++ imperialcode/home/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$',views.home)
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
