25412fa8f5574aead18341b1a4d6392d35e18c16
ht sun <sunhttt@gmail.com>
2020-05-26 10:57:48 +0800

    updated database details

--- imperialcode/imperialcode/settings.py
+++ imperialcode/imperialcode/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'imperialcode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase', #to be changed
+        'USER': 'g1927140_u',
+        'PASSWORD': 'zGpqzMXl37',
+        'HOST': 'db.doc.ic.ac.uk',
+        'PORT': '5432',
     }
 }
 
