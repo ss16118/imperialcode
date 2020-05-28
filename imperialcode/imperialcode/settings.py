7d855eadf50ff438c0a22e83148fdce74018785b
Gary <gykawesome@163.com>
2020-05-28 19:16:53 +0800

    changed settings.py

--- imperialcode/imperialcode/settings.py
+++ imperialcode/imperialcode/settings.py
@@ -133,9 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-
+# Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
