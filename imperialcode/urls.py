067e19c3a4c7fbb68b044317ce900c9aa09cdf42
ss16118 <siyuan.shen18@imperial.ac.uk>
2020-05-30 17:59:43 +0100

    Static files now displayed correctly in production mode e.g. DEBUG set to False

--- imperialcode/urls.py
+++ imperialcode/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
-
+from django.conf.urls import include, url
+from django.views.static import serve
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
