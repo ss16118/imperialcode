16be6a85666d28813fe2c703abc836ce55d8fd43
ss16118 <siyuan.shen18@imperial.ac.uk>
2020-05-30 15:04:23 +0100

    Changed file storage settings

--- imperialcode/settings.py
+++ imperialcode/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1%q&8f1#!7j#e0hroe@h*182*fu$%_x&w&$cs@y4g6z)pub*@c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'imperialcode.herokuapp.com']
 
@@ -170,6 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
@@ -178,7 +179,7 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
