3b93fec38c5dae61447e9a6b3eb35c4c30238e23
ht sun <sunhttt@gmail.com>
2020-06-01 22:55:35 +0800

    added models for forum

--- home/models.py
+++ home/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 # class User(models.Model):
 #     uname = models.CharField(max_length=30)
+class Forum(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
