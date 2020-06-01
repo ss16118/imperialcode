f8e36331bf8cc84c11cd54734da791b13eea973d
ss16118 <siyuan.shen18@imperial.ac.uk>
2020-06-01 16:09:45 +0100

    Reimplemented most functionailities with CSS and JS for question solving page

--- static/files/question_solving_page/data.js
+++ static/files/question_solving_page/data.js
@@ -200,19 +200,133 @@ $axure.loadCurrentPage(
         return _creator();
     })());
 */
+const EDITOR_DEFAULT_WIDTH = 574;
+const EDITOR_DEFAULT_HEIGHT = 357;
+const EDITOR_EXPAND_WIDTH = 1268;
+const EDITOR_EXPAND_HEIGHT = 581;
+const DEFAULT_X = 713;
+const EXPAND_X = 19;
+
+function testcaseConsoleOnclick() {
+    let testcaseButton = document.getElementById("u269_div");
+    let outputButton = document.getElementById("u270_div");
+
+    outputButton.className = outputButton.className.replace(" selected", "");
+    testcaseButton.className = testcaseButton.className.replace(" selected", "");
+    testcaseButton.className += " selected";
+
+    let testcaseArea = document.getElementById("u274");
+    let outputArea = document.getElementById("u277");
+
+    outputArea.style.display = "none";
+    testcaseArea.style.display = "flex";
+}
+
+function outputConsoleOnclick() {
+    let testcaseButton = document.getElementById("u269_div");
+    let outputButton = document.getElementById("u270_div");
+
+    testcaseButton.className = testcaseButton.className.replace(" selected", "");
+    outputButton.className = outputButton.className.replace(" selected", "");
+    outputButton.className += " selected";
+
+    let outputArea = document.getElementById("u277");
+    let testcaseArea = document.getElementById("u274");
+
+    testcaseArea.style.display = "none";
+    outputArea.style.display = "flex";
+}
+
 
 function hideConsoleOnClick() {
-    let hidden = document.getElementById("u282_text").innerHTML.localeCompare("Hide console ⯅") == 0;
+    let buttonText = document.getElementById("u282_text");
+    let hidden = buttonText.innerHTML.localeCompare("Hide console ⯅") == 0;
 
     let editor = $('.CodeMirror')[0].CodeMirror;
-    console.log(hidden);
+
     if (hidden) {
-        editor.setSize("574", "357");
-        document.getElementById("u282_text").innerHTML = "Hide console ⯆";
+        editor.setSize(null, EDITOR_DEFAULT_HEIGHT);
+        buttonText.innerHTML = "Hide console ⯆";
     } else {
-        editor.setSize("574", "581");
-        document.getElementById("u282_text").innerHTML = "Hide console ⯅";
+        editor.setSize(null, EDITOR_EXPAND_HEIGHT);
+        buttonText.innerHTML = "Hide console ⯅";
     }
 
     editor.refresh();
+}
+
+function hideSpecOnClick() {
+    let buttonText = document.getElementById("u281_text");
+    let hidden = buttonText.innerHTML.localeCompare("⯈") == 0;
+    let wrapper = document.getElementById("u278");
+    let spec = document.getElementById("u279");
+    let singleQuestion = document.getElementById("u280");
+
+    let testcaseButton = document.getElementById("u269");
+    let outputButton = document.getElementById("u270");
+    let testcaseArea = document.getElementById("u274");
+    let outputArea = document.getElementById("u277");
+
+    let editor = $('.CodeMirror')[0].CodeMirror;
+
+    if (hidden) {
+        editor.setSize(EDITOR_DEFAULT_WIDTH, null);
+
+        spec.style.visibility = "visible";
+        singleQuestion.style.visibility = "visible";
+
+        wrapper.style.left = DEFAULT_X + "px";
+        wrapper.style.width = EDITOR_DEFAULT_WIDTH + "px";
+
+        buttonText.innerHTML = "⯇";
+
+        testcaseButton.style.left = DEFAULT_X + "px";
+        outputButton.style.left = "817px";
+        testcaseArea.style.left = DEFAULT_X + "px";
+        resizeTextAreaWidth(testcaseArea.id, EDITOR_DEFAULT_WIDTH);
+        outputArea.style.left = DEFAULT_X + "px";
+        resizeTextAreaWidth(outputArea.id, EDITOR_DEFAULT_WIDTH);
+
+    } else {
+        editor.setSize(EDITOR_EXPAND_WIDTH, null);
+
+        spec.style.visibility = "hidden";
+        singleQuestion.style.visibility = "hidden";
+
+        wrapper.style.left = "19px";
+        wrapper.style.width = EDITOR_EXPAND_WIDTH + "px";
+
+        buttonText.innerHTML = "⯈";
+
+        testcaseButton.style.left = EXPAND_X + "px";
+        outputButton.style.left = "124px";
+        testcaseArea.style.left = EXPAND_X + "px";
+        resizeTextAreaWidth(testcaseArea.id, EDITOR_EXPAND_WIDTH);
+        outputArea.style.left = EXPAND_X + "px";
+        resizeTextAreaWidth(outputArea.id, EDITOR_EXPAND_WIDTH);
+    }
+
+    editor.refresh();
+}
+
+function showSubquestionsOnClick() {
+    let subquestionsPanel = document.getElementById("u328");
+    subquestionsPanel.style.display = "flex";
+    subquestionsPanel.style.visibility = "visible";
+}
+
+window.onclick = function(event) {
+    let subquestionsPanel = document.getElementById("u328");
+    if (event.target == subquestionsPanel) {
+        subquestionsPanel.style.display = "none";
+    }
+}
+
+let resizeTextAreaWidth = function(componentId, newWidth) {
+    let component = document.getElementById(componentId);
+    component.style.width = newWidth + "px";
+    let componentDiv = document.getElementById(componentId + "_div");
+    componentDiv.style.width = newWidth + "px";
+    let componentInput = document.getElementById(componentId + "_input");
+    componentInput.style.width = newWidth + "px";
 }
