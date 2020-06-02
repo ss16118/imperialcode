48fcff28f052b668e5ded3f386d204af35e3b905
Gary <gykawesome@163.com>
2020-06-02 23:53:04 +0800

    registered Posts and Comments in admin

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from home.models import PastPaper, Question
+from forum.models import Post, Comment
+
 # Register your models here.
 admin.site.register(PastPaper)
 admin.site.register(Question)
+admin.site.register(Post)
+admin.site.register(Comment)
