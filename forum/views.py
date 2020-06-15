6529aab228dbe90fb7a69aeae84c666c17fb82d9
ss16118 <siyuan.shen18@imperial.ac.uk>
2020-06-15 01:27:19 +0100

    Deleted incomplete line

--- forum/views.py
+++ forum/views.py
@@ -82,7 +82,6 @@ class ForumDetailView(DetailView):
         post.views += 1
         post.save()
         comments = Comment.objects.filter(forum_id=post.id).order_by("-created_at")
-        main_post_vote =
         context = {"post": post, "comments": comments}
         return render(request, "forum/post_detail.html", context=context)
 
