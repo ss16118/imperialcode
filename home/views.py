0346548c13747eeea57f6544472e703ac2a1588e
Gary <gykawesome@163.com>
2020-06-17 12:19:59 +0800

    remove usage of comment.upvotes to avoid error in clicking 'discuss'

--- home/views.py
+++ home/views.py
@@ -282,7 +282,7 @@ def question_comment_page(request):
             "user": comment.user,
             "title": comment.title,
             "create_at": comment.created_at,
-            "upvotes": comment.upvotes,
+            "upvotes": 0,
             "views": comment.views,
             "num_comments": __get_sub_comment_num(comment.id)
         })
